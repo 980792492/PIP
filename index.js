120be8ddea6c1c65491534034ca16f3c0cc4d18c
tangjunhao <980792492@qq.com>
2023-11-24 16:00:35 +0800

    feat: test

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: tangjunhao 980792492@qq.com
+ * @Date: 2023-11-24 16:00:00
+ * @LastEditors: tangjunhao 980792492@qq.com
+ * @LastEditTime: 2023-11-24 16:00:07
+ * @FilePath: /pip/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log('111')
